31843e48e90bc830ae9f8076d8a53add086e145f
red-sprout <jjw05015@gmail.com>
2024-02-26 18:17:26 +0900

    feat : 바둑돌 상태(BLACK, WHITE, BLANK) enum

---
+++ sixmok/src/main/java/sixmok/domain/Dol.java
@@ -0,0 +1,17 @@
+package sixmok.domain;
+
+public enum Dol {
+	BLACK('●'),
+	WHITE('○'),
+	BLANK('·');
+	
+	private char dol;
+	
+	Dol(char dol) {
+		this.dol = dol;
+	}
+	
+	public char getDol() {
+		return dol;
+	}
+}
